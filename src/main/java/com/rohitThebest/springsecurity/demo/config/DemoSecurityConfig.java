19c8dbf69e2252443c3f7f9cd8403d4fa08e7da3
Rohit <rkumar8092378845@gmail.com>
2021-10-26 23:23:23 +0530

    initial commit 2

--- src/main/java/com/rohitThebest/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/rohitThebest/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.rohitThebest.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -24,4 +25,10 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 				.withUser(users.username("sagar").password("test123").roles("ADMIN"));
 	}
 
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		
+	}
 }
